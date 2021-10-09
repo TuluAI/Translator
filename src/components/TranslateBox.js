457d39957d6a6e8ccd74237e250197568ed464d8
Amrit Shenava <amritshenava98@users.noreply.github.com>
2021-10-09 15:56:46 +0000

    FromLang

--- src/components/TranslateBox.js
+++ src/components/TranslateBox.js
@@ -1,30 +1,77 @@
 import React, {useRef, useState} from 'react';
-import { Form, Alert } from 'react-bootstrap';
+import { Dropdown, DropdownButton, Form, Alert } from 'react-bootstrap';
 import tuluwords from '../utils/tuluwords.json';
 import '../App.css';
 
 function  TranslateBox(){
   //const toTranslate = useRef();
+  const [fromLanguage, setFromLanguage] = useState();
+  const [toLanguage, setToLanguage] = useState();
+  const [language, setLanguage] = useState();
   const [translatedWord, setTranslatedWord] = useState();
   const [error, setError] = useState(false);
   const [warning, setWarning] = useState();
+  const fromLangRef = useRef();
   //const [toTranslate, setToTranslate] = useState();
   const handleChange = (e) => {
     e.preventDefault();
-    const word = tuluwords.find((w) => w.english === e.target.value.toLowerCase().trim()); 
+    if(fromLangRef.current.value === "English"){
+      console.log(e.target.value);
+      const word = tuluwords.find((w) => w.english === e.target.value.toLowerCase().trim()); 
+
+      if(word){
+        setTranslatedWord(word.tulu);
+        setError(false);
+      }
+
+      else if(e.target.value === ""){
+      setError(false);
+      }
+      else {
+        setTranslatedWord("");
+        setError(true);
+        setWarning("Translation is not available for this word as we are in still in beta and working on being able to translate all words and sentences.");
+      }
+    }
+    else if(fromLangRef.current.value === "Hindi"){
+      const word = tuluwords.find((w) => w.hindi === e.target.value.toLowerCase().trim()); 
+
+      if(word){
+        setTranslatedWord(word.tulu);
+        setError(false);
+      }
+      else if(e.target.value === ""){
+        setError(false);
+      }
+      else {
+        setTranslatedWord("");
+        setError(true);
+        setWarning("Translation is not available for this word as we are in still in beta and working on being able to translate all words and sentences.");
+      }
+    }
+
+    else if(fromLangRef.current.value === "Marathi"){
+      const word = tuluwords.find((w) => w.marathi === e.target.value.toLowerCase().trim());
+
+      if(word){
+        setTranslatedWord(word.tulu);
+        setError(false);
+      }
+      else if(e.target.value === ""){
+        setError(false);
+      }
+      else {
+        setTranslatedWord("");
+        setError(true);
+        setWarning("Translation is not available for this word as we are in still in beta and working on being able to translate all words and sentences.");
+      }
+    }
     //const noword = tuluwords.find((w) => w.english !== e.target.value.toLowerCase().trim());
-    if(word){
-      setTranslatedWord(word.tulu);
-      setError(false);
-    }
-    else if(e.target.value === ""){
-      setError(false);
-    }
-    else {
-      setTranslatedWord("");
-      setError(true);
-      setWarning("Translation is not available for this word as we are in still in beta and working on being able to translate all words and sentences.");
-    }
+  }
+
+  const handleFromLanguage = (e) => {
+    e.preventDefault();
+    setFromLanguage(e.target.value);
   }
 
   return(
@@ -33,8 +80,19 @@ function  TranslateBox(){
       <Form> 
         {error ? <Alert variant="danger">{warning}</Alert> : null}
         <Form.Group>
-          <Form.Label>English</Form.Label>
-          <Form.Control size="lg" type="text" onChange={handleChange}/>
+            {/* <DropdownButton id="basic-dropdown-button" title="Translate From" onChange={handleFromLanguage}>
+              <Dropdown.Item>English</Dropdown.Item>
+              <Dropdown.Item>Hindi</Dropdown.Item>
+              <Dropdown.Item>Kannada</Dropdown.Item>
+            </DropdownButton> */}
+          <Form.Label>Translate From</Form.Label>
+          <Form.Control as="select" ref={fromLangRef}>
+            <option>Select Language</option>
+            <option value="English">English</option>
+            <option value="Hindi">Hindi</option>
+            <option value="Marathi">Marathi</option>
+          </Form.Control>  
+          <Form.Control size="lg" type="text" onChange={handleChange} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Tulu</Form.Label>
